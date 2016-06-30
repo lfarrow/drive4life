3fa9be5058e7915853003dd5899a4d263f2934e9
Lewis Farrow <lewis.farrow@myfuturesbright.com>
2016-06-30 16:24:30 +0100

    Switched over to minified css

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ var gulp = require('gulp');
 var concat = require("gulp-concat");
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
+var autoprefixer = require('gulp-autoprefixer');
+var rename = require('gulp-rename');
+var minifyCSS = require('gulp-minify-css');
 
 /*
 	Sass
@@ -9,7 +12,14 @@ var watch = require('gulp-watch');
 gulp.task('styles', function() {
 	gulp.src('assets/css/sass/styles.scss')
 		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('assets/css/'))
+		// .pipe(autoprefixer('last 6 versions'))
+		.pipe(gulp.dest('assets/css/'));
+	gulp.src('assets/css/sass/styles.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(minifyCSS())
+		// .pipe(autoprefixer('last 6 versions'))
+        .pipe(rename('styles.min.css'))
+		.pipe(gulp.dest('assets/css/'));
 });
 
 gulp.task('watchScss', function(){
